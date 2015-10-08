c49a0e9176e340518dda477b20b19c17c2c0b3cc
Lukasz_Sobieraj <lukaszsobieraj@tmsolution.pl>
2015-10-08 15:20:13 +0200

    Merge origin/master
    Conflicts:
    	Resources/doc/Instalacja.md

---
+++ Form/DualListBoxType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace TMSolution\FormTypeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\AbstractType as FormAbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+// For Symfony 2.1 and higher:
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DualListBoxType extends AbstractType {
+
+    public function getParent() {
+        return 'entity';
+    }
+
+    public function getName() {
+        return 'duallistbox';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'lazyjs' => true,
+           
+        ));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options) {
+        $view->vars['lazyjs'] = $options['lazyjs'];
+      
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->setAttribute('lazyjs', $options['lazyjs'])
+              
+
+        ;
+    }
+
+    
+
+}
